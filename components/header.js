a72ab44f0f20984330a295a92511c6072c5ca193
Simon Huang <simon.huang@tum.de>
2022-09-12 21:48:38 +0200

    style using daisyui

--- components/header.js
+++ components/header.js
@@ -1,39 +1,69 @@
 import Image from "next/image";
 
 import monogram from "../assets/photos/Monogram.png";
-import styles from "../styles/header.module.css";
 
-const Header = (props) => (
-  <div className={styles.headerContainer}>
-    <a
-      className="navbar-brand fw-bold d-flex flex-row align-text-top"
-      href="#"
-      style={{ color: "#eeeae3" }}
-    >
-      <Image src={monogram} alt="" width="40" height="35" className="rounded" />
-    </a>
-    <ul className="nav  justify-content-end ">
-      <li className="nav-item">
-        <a className="nav-link link-dark fw-bolder" href="#work">
-          Work
+const menuItems = [
+  { name: "Work", link: "#work" },
+  { name: "Education", link: "#education" },
+  { name: "Skills", link: "#skills" },
+  { name: "Projects", link: "#projects" },
+];
+
+const Header = () => (
+  <>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className="btn btn-ghost normal-case text-xl">
+          <Image
+            src={monogram}
+            alt=""
+            width="40"
+            height="35"
+            className="rounded"
+          />
         </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link link-dark fw-bolder" href="#education">
-          Education
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link link-dark fw-bolder" href="#skills">
-          Skills
-        </a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link link-dark fw-bolder" href="#Projects">
-          Projects
-        </a>
-      </li>
-    </ul>
-  </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">
+          {menuItems.map((item) => (
+            <li key={item.name}>
+              <a href={item.link}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </>
 );
 export default Header;
--- components/projects.js
+++ components/projects.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ProjectCarousel from "./projectCarousel";
 import styles from "../styles/projects.module.css";
+
 function projects(props) {
   return (
-    <div id="Projects" className={styles.projectsContainer} data-aos="fade-up">
+    <div id="projects" className={styles.projectsContainer} data-aos="fade-up">
       <h2 style={{ textAlign: "center", marginBottom: 20 }}>Projects</h2>
       <ProjectCarousel />
     </div>
