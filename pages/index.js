6d76cabf494a894a991045266b1616f7d901b987
Simon Huang <simon.huang@tum.de>
2022-09-13 00:21:23 +0200

    add footer

--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,12 @@ export default function Home() {
         <Skills />
         <Projects />
       </main>
+
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <div>
+          <p>Powered by Vercel</p>
+        </div>
+      </footer>
     </div>
   );
 }
