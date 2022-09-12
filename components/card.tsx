a0a773d413cb4808ca8bf3ab262ec77ef4e8076c
Simon Huang <simon.huang@tum.de>
2022-09-12 21:48:17 +0200

    style work using daisyui

---
+++ components/card.tsx
@@ -0,0 +1,26 @@
+interface cardProps {
+  title: string;
+  subtitle: string;
+  location: string;
+  date: string;
+  description: string;
+  className?: string;
+}
+
+const Card = (props: cardProps) => (
+  <div
+    data-aos="fade-right"
+    className={"card bg-base-100 shadow-xl " + props.className}
+  >
+    <div className="card-body">
+      <h2 className="card-title">{props.title}</h2>
+      <p className="mb-2">{props.subtitle}</p>
+      <p className="mb-2">
+        {props.location}, {props.date}
+      </p>
+      {props.description && <p>{props.description}</p>}
+    </div>
+  </div>
+);
+
+export default Card;
